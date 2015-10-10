96ce9464f9d7ffb02c0f59b28b03180177807f2f
Jason Perrin <jasonvperrin@gmail.com>
2015-10-10 11:44:45 -0700

    Add button for testing instead of autoplaying audio

--- cena.js
+++ cena.js
@@ -1,28 +1,30 @@
 $(document).ready(function() {
-  var introAudio = new Audio('intro.mp3');
-  var loopAudio = new Audio('loop.mp3');
-  introAudio.load();
-  loopAudio.load();
+  $("#button").click(function() {
+    var introAudio = new Audio('intro.mp3');
+    var loopAudio = new Audio('loop.mp3');
+    introAudio.load();
+    loopAudio.load();
 
-  function start() {
-    introAudio.play();
-    introAudio.addEventListener('loadedmetadata', function() {
-      setTimeout(startLoop, (introAudio.duration * 1000));
-    });
-  }
+    function start() {
+      introAudio.play();
+      introAudio.addEventListener('loadedmetadata', function() {
+        setTimeout(startLoop, (introAudio.duration * 1000));
+      });
+    }
 
-  function startLoop() {
-    introAudio.pause();
-    loopAudio.play();
-    setInterval(loopMusic, (loopAudio.duration * 1000) - 50);
-  }
+    function startLoop() {
+      introAudio.pause();
+      loopAudio.play();
+      setInterval(loopMusic, (loopAudio.duration * 1000) - 50);
+    }
 
-  function loopMusic() {
-    loopAudio.pause();
-    loopAudio.currentTime = 0;
-    loopAudio.play();
-  }
+    function loopMusic() {
+      loopAudio.pause();
+      loopAudio.currentTime = 0;
+      loopAudio.play();
+    }
 
-  start();
+    start();
+  });
 });
 
